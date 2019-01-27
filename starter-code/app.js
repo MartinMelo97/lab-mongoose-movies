d5efefece273fa02202c5f611c563d47df121a92
Martin Melo <nitram-210397@hotmail.com>
2019-01-27 15:57:47 -0600

    app.js updated with the correct DB and using pug as engine view!

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,14 +4,14 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
+const hbs          = require('pug');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -40,7 +40,7 @@ app.use(require('node-sass-middleware')({
       
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
